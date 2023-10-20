71d07ec8b5d9d733b7307b08a9f3271206c994fd
Mehedi5271 <mhassan201205@bscse.uiu.ac.bd>
2023-10-21 02:40:32 +0600

    29 class 10% done

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -2,11 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\CartProduct;
 use Illuminate\Http\Request;
 
 class CartController extends Controller
 {
     public function store(Request $request){
-        dd($request->all());
+        // dd($request->all());
+        CartProduct::create([
+            'user_id'=> auth()->user()->id,
+            'product_id'=> $request->product_id,
+            'quantity'=> $request->quantity,
+            'color_id'=> $request->color_id,
+        ]);
+        dd('created');
     }
 }
