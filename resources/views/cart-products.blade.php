115b97beff2b76a6e274fcf938623ab40c209cbd
Mehedi5271 <mhassan201205@bscse.uiu.ac.bd>
2023-10-28 02:10:22 +0600

    31 number class 50%  done

--- resources/views/cart-products.blade.php
+++ resources/views/cart-products.blade.php
@@ -9,22 +9,47 @@
             <th>Serial</th>
             <th>Product</th>
             <th>Quantity</th>
-            <th>Price</th>
+            <th class="text-center">Price</th>
         </thead>
         <tbody>
             @foreach (auth()->user()->cartProducts as $cartProduct)
                 
             <tr>
-                <td><span class="btn  btn-sm btn-danger me-2 remove-btn">x</span></td>
+                <td><span class="btn  btn-sm btn-danger me-2 remove-btn" data-id="{{$cartProduct->id}}">x</span></td>
                 <td>  {{$loop->iteration}} </td>
                 <td>{{$cartProduct->product->title}} {{$cartProduct->color ? '-'.$cartProduct->color->name:null}} </td>
-                <td> {{$cartProduct->quantity}} </td>
-                <td> {{$cartProduct->product->price}} </td>
+                <td > {{$cartProduct->quantity}} </td>
+                <td class="price text-end"> {{$cartProduct->product->price}} </td>
             </tr>
         </tbody>
         @endforeach
+        <tr>
+            <td colspan="4"></td>
+            <td class="text-end">Total: <span id="total_price">0</span></td>
+        </tr>
     </table>
-    <script>
-        alert('hello');
-    </script>
+   @push('script')
+   <script>
+    const removeBtn = document.querySelectorAll('.remove-btn');
+    removeBtn.forEach(function(btn){
+        btn.addEventListener('click', function(){
+            btn.parentElement.parentElement.remove()
+            alert('deleted')
+            updateTotalPrice()
+        })            
+    }) 
+    updateTotalPrice()
+   function updateTotalPrice()
+   {
+    const priceElement = document.querySelectorAll('.price');
+    let totalPrice = 0; 
+    priceElement.forEach(function(element){
+        totalPrice += parseFloat(element.innerText);
+      document.getElementById('total_price').innerText=totalPrice
+    }) 
+   }  
+
+     </script>
+       
+   @endpush
 </x-master>
